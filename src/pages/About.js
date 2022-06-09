32a51817a68e3182e50b2c3dafdb84c15b349e3c
pazricar <pazricar@msu.edu>
2022-06-09 18:55:11 -0400

    Created a Split About Page with Pic and a Todo input in the Home Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import "./About.css";
+import profile_pic_name from "../assets/profile_pic_name.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+             <div>
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              // Image goes here   
+              src = {profile_pic_name}      
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Ricardo Paz</div>
+            <div className="brief_description">
+              // Details about you goes here
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     )
   }
